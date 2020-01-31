3ce650663c0cf2f6551e9f3d813c691b13e762bf
Tarcísio Genaro Rodrigues <tarcisio.genaro@gmail.com>
2020-01-31 14:07:34 -0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -12,9 +12,16 @@ where cmake > NUL 2> NUL
 if NOT '%ERRORLEVEL%'=='0' goto fail
 
 
+if NOT "%1"=="" (
+	set tmp=%1
+) else (
+	set tmp=tmp
+)
+
+
 set script_dir=%~dp0
 
-set gen_dir=%script_dir%Multi\
+set gen_dir=%script_dir%%tmp%\
 
 if EXIST %gen_dir% (
 	set error_message="rmdir /s/q %gen_dir%"
@@ -27,23 +34,11 @@ set error_message="mkdir %gen_dir%"
 mkdir %gen_dir%
 if NOT '!ERRORLEVEL!'=='0' goto fail
 
-pushd %gen_dir%
+cd %gen_dir%
 
 
-set error_message="conan install .. -g cmake_multi"
-
-conan install .. -g cmake_multi -s arch=x86_64 -s build_type=Release -s compiler.runtime=MT -s compiler.version=16 --build=missing
-if NOT '%ERRORLEVEL%'=='0' goto fail 
-
-conan install .. -g cmake_multi -s arch=x86_64 -s build_type=Debug -s compiler.runtime=MTd -s compiler.version=16 --build=missing
-if NOT '%ERRORLEVEL%'=='0' goto fail 
-
-
-set generator=Visual Studio 16 2019
-
-set error_message="cmake .. -G "%generator%" -DMULTI_CONFIG=ON -A x64"
-cmake .. -G "%generator%" -DMULTI_CONFIG=ON -A x64
-
+set error_message="conan install .. --build=missing"
+conan install .. --build=missing
 if NOT '%ERRORLEVEL%'=='0' goto fail 
 
 
